b1f66d10f8ee1aa6f03f6dd2224f393038ad1b58
MysteriouBaboon <MysteriousBaboon@hotmail.com>
2020-07-22 18:27:25 +0200

    Ui and new enemy sprite

---
+++ Assets/Scripts/Script_Score.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Script_Score : MonoBehaviour
+{
+    Text score;
+    public int enemies;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        score = GetComponent<Text>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject[] gos = GameObject.FindGameObjectsWithTag("Enemy");
+        enemies = gos.Length;
+        score.text = enemies.ToString();
+    }
+}
