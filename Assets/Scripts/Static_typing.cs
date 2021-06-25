4c84d0e362ac64c1566f4ef6e73ca0a1254e9efb
MysteriouBaboon <MysteriousBaboon@hotmail.com>
2021-06-25 10:57:04 +0200

    Dead

---
+++ Assets/Scripts/Static_typing.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+int var1;
+bool var2;
+string var3;
+
+int variable = 5;
+public int variable2 = 1;
+
+// Start is called before the first frame update
+void Start()
+{
+    
+}
+
+// Update is called once per frame
+void Update()
+{
+    print(var3);
+}
